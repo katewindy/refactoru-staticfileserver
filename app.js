294bc81306c4a4b7b78598dafc677761c99e0ae0
Kate Windheuser <roboticbrain@gmail.com>
2014-10-14 12:38:20 -0600

    finished this project

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	res.render('index');
+	fs.readFile('data.txt', function(err, data){
+
+  		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});
+
 });
 
 var server = app.listen(4987, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
+
+
